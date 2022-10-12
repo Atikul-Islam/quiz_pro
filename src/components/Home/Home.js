c22e2954f0cf0a338f633e1850662efdc03c4692
Atikul-Islam <atikmina25@gmail.com>
2022-10-12 19:44:38 +0600

    added banner

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import Topics from '../Topics/Topics';
+import logo from './quiz.jpg'
+import './Home.css'
+
 const Home = () => {
     return (
         <div>
-            <h1>this is home</h1>
+            <div className='banner'>
+                <img src={logo} alt="" />
+                <h1>“Isn’t life a collection of weird quizzes with no answers to half the questions?”</h1>
+            </div>
         <Topics></Topics>
         </div>
     );
