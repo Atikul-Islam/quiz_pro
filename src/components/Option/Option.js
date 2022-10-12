f10662829a3811ce2a35f6dbaa99c518b1840bc3
Atikul-Islam <atikmina25@gmail.com>
2022-10-12 19:33:33 +0600

    added toast in correct answer

--- src/components/Option/Option.js
+++ src/components/Option/Option.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import { toast, ToastContainer } from 'react-toastify';
 
 const Option = ({opt}) => {
+
+    const showToast = () =>{
+        toast()
+
+    }
     // const{opt}=opt;
     // console.log(opt)
     return (
         <div>
             <input type="radio" id="html" name="fav_language" value={opt}/>
-            <label htmlFor="html">{opt}</label> 
+            <label onClick={showToast} htmlFor="html">{opt}</label> 
+
+            <ToastContainer />
         </div>
+        
     );
 };
 
--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,16 +1,26 @@
 import React from 'react';
+import { toast, ToastContainer } from 'react-toastify';
 import Option from '../Option/Option';
 
 const Question = ({qus}) => {
-    // console.log(qus)
+    const {correctAnswer,options}=qus;
+    const showToast = () =>{
+        return toast({correctAnswer},{
+            position:"top-center"
+        });
+    }
+    console.log(qus)
     return (
         <div>
             <h2>{qus.question}</h2>
             {
-                qus.options.map(opt => <Option 
+                options.map(opt => <Option 
                 opt={opt}
                 ></Option>)
             }
+            <br />
+            <button onClick={showToast}>see answer</button>
+            <ToastContainer />
         </div>
     );
 };
