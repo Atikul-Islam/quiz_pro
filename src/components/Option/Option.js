447714b063613044ad35cf7a02b154822073f59b
Atikul-Islam <atikmina25@gmail.com>
2022-10-12 21:31:56 +0600

    added toast for option

--- src/components/Option/Option.js
+++ src/components/Option/Option.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-const Option = ({opt}) => {
-
+const Option = ({opt,ca}) => {
     const showToast = () =>{
-        toast()
+        if(ca===opt){
+            toast('Answer is correct',{
+                position:'top-center'
+            })
+        }
+        else{
+            toast('Answer is wrong',{
+                position:'top-center'
+            })
+        }
+   
 
     }
-    // const{opt}=opt;
-    // console.log(opt)
+
     return (
         <div>
-            <input type="radio" id="html" name="fav_language" value={opt}/>
+            <input type="radio" onClick={showToast} id={opt} name="fav_language" value={opt}/>
             <label onClick={showToast} htmlFor="html">{opt}</label> 
 
             <ToastContainer />
--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import { toast, ToastContainer } from 'react-toastify';
 import Option from '../Option/Option';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Question = ({qus}) => {
     const {correctAnswer,options}=qus;
     const showToast = () =>{
-        toast({correctAnswer},{
-            position:"top-center"
+        toast(`${correctAnswer}`,{
+            position: 'top-center'
         });
     }
-    console.log(qus)
     return (
         <div>
             <h2>{qus.question}</h2>
             {
                 options.map(opt => <Option 
                 opt={opt}
+                ca={correctAnswer}
                 ></Option>)
             }
             <br />
